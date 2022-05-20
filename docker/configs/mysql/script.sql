8acb4f8336e744807d44bb14434f7489c2d0f7f9
Agustin Alvarez <agustin_alvarez@edsa.com.ar>
2022-05-20 18:32:20 -0300

    scripts.sql actualizado

--- docker/configs/mysql/script.sql
+++ docker/configs/mysql/script.sql
@@ -1,3 +1,3 @@
 use devops_app;
 create table welcome (id int not null AUTO_INCREMENT, description varchar(255) not null, PRIMARY KEY (id));
-insert into welcome (description) values ('prueba 1'),('prueba 2');
+insert into welcome (description) values ('Alvarez, Agustin'),('Leiva, Santiago Joaquín'),('Olivera, Lautaro'),('Otero, Franco'),('Palavecino, Walter Daniel'),('Rodriguez, Marcelo Tony'),('Stadler, Marcelo');
