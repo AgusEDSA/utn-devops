bcbaa3fb21f183caba56459d9e9ca72718defd00
Santiago Leiva <santiagojoaquinleiva@gmail.com>
2022-05-28 11:19:37 -0300

    Unidad 3

--- Vagrant.bootstrap.sh
+++ Vagrant.bootstrap.sh
@@ -3,7 +3,9 @@
 ### Aprovisionamiento de software ###
 
 # Actualizo los paquetes de la maquina virtual
-sudo apt-get update
+sudo apt-get update -y
+
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common linux-image-extra-virtual-hwe-$(lsb_release -r |awk  '{ print $2 }') linux-image-extra-virtual
 
 #Desintalo el servidor web instalado previamente en la unidad 1
 if [ -x "$(command -v apache2)" ];then
@@ -17,6 +19,10 @@ fi
 
 if [ -f "/tmp/ufw" ]; then
 	sudo mv -f /tmp/ufw /etc/default/ufw
+fi
+
+if [ -f "/tmp/etc_hosts.txt" ]; then
+	sudo mv -f /tmp/etc_hosts.txt /etc/hosts
 fi
 
 ### Configuración del entorno ###
@@ -49,22 +55,65 @@ if [ ! -d "$APP_PATH" ]; then
 	sudo git checkout unidad-2
 fi
 
+# Puppet
+PUPPET_DIR="/etc/puppet"
+ENVIRONMENT_DIR="${PUPPET_DIR}/code/environments/production"
+PUPPET_MODULES="${ENVIRONMENT_DIR}/modules"
+if [ ! -x "$(command -v puppet)" ]; then
+  #configuración de repositorio
+	sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+ 	sudo apt-get update
+	sudo apt install -y puppetmaster
 
-######## Instalacion de DOCKER ########
-if [ ! -x "$(command -v docker)" ]; then
-	sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+	#### Instalacion puppet agent
+	sudo apt install -y puppet
 
-	##Configuramos el repositorio
-	curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" > /tmp/docker_gpg
-	sudo apt-key add < /tmp/docker_gpg && sudo rm -f /tmp/docker_gpg
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  # Esto es necesario en entornos reales para posibilitar la sincronizacion
+  # entre master y agents
+	sudo timedatectl set-timezone America/Argentina/Buenos_Aires
+	sudo apt-get -y install ntp
+	sudo systemctl restart ntp
 
-	#Actualizo los paquetes con los nuevos repositorios
-	sudo apt-get update -y
+  # Muevo el archivo de configuración de Puppet al lugar correspondiente
+  sudo mv -f /tmp/puppet-master.conf $PUPPET_DIR/puppet.conf
 
-	#Instalo docker desde el repositorio oficial
-	sudo apt-get install -y docker-ce docker-compose
+  # elimino certificados de que se generan en la instalación.
+  # no nos sirven ya que el certificado depende del nombre que se asigne al maestro
+  # y en este ejemplo se modifico.
+  sudo rm -rf /var/lib/puppet/ssl
 
-	#Lo configuro para que inicie en el arranque
-	sudo systemctl enable docker
+  # Agrego el usuario puppet al grupo de sudo, para no necesitar password al reiniciar un servicio
+  sudo usermod -a -G sudo,puppet puppet
+
+  # Estructura de directorios para crear el entorno de Puppet
+  sudo mkdir -p $ENVIRONMENT_DIR/{manifests,modules,hieradata}
+  sudo mkdir -p $PUPPET_MODULES/docker_install/{manifests,files}
+
+  # Estructura de directorios para crear el modulo de Jenkins
+  sudo mkdir -p $PUPPET_MODULES/jenkins/{manifests,files}
+
+  # muevo los archivos que utiliza Puppet
+  sudo mv -f /tmp/site.pp $ENVIRONMENT_DIR/manifests #/etc/puppet/manifests/
+  sudo mv -f /tmp/init.pp $PUPPET_MODULES/docker_install/manifests/init.pp
+  sudo mv -f /tmp/env $PUPPET_MODULES/docker_install/files
+  sudo mv -f /tmp/init_jenkins.pp $PUPPET_MODULES/jenkins/manifests/init.pp
+  sudo cp /usr/share/doc/puppet/examples/etckeeper-integration/*commit* $PUPPET_DIR
+  sudo chmod 755 $PUPPET_DIR/etckeeper-commit-p*
 fi
+
+
+sudo ufw allow 8140/tcp
+
+# al detener e iniciar el servicio se regeneran los certificados
+echo "Reiniciando servicios puppetmaster y puppet agent"
+sudo systemctl stop puppetmaster && sudo systemctl start puppetmaster
+sudo systemctl stop puppet && sudo systemctl start puppet
+
+
+# limpieza de configuración del dominio utn-devops.localhost es nuestro nodo agente.
+# en nuestro caso es la misma máquina
+sudo puppet node clean utn-devops.localhost
+
+# Habilito el agente
+sudo puppet agent --certname utn-devops.localhost --enable
+
--- Vagrantfile
+++ Vagrantfile
@@ -1,11 +1,13 @@
 # GRUPO 7
 Vagrant.configure("2") do |config|
 
-  config.vm.box = "ubuntu/xenial64"
+  config.vm.box = "hashicorp/bionic64"
 
 
   config.vm.network "forwarded_port", guest: 8080, host: 8080
   config.vm.network "forwarded_port", guest: 4400, host: 4400
+  config.vm.network "forwarded_port", guest: 8082, host: 8082
+  config.vm.network "forwarded_port", guest: 8140, host: 8140
 
 
   config.vm.hostname = "utn-devops.localhost"
@@ -24,8 +26,15 @@ Vagrant.configure("2") do |config|
   end
 
   config.vm.provision "file", source: "hostConfigs/ufw", destination: "/tmp/ufw"
+  config.vm.provision "file", source: "hostConfigs/etc_hosts.txt", destination: "/tmp/etc_hosts.txt"
 
-  config.vm.provision :shell, path: "Vagrant.bootstrap.sh", run: "always"
+  config.vm.provision "file", source: "hostConfigs/puppet/site.pp", destination: "/tmp/site.pp"
+  config.vm.provision "file", source: "hostConfigs/puppet/init.pp", destination: "/tmp/init.pp"
+  config.vm.provision "file", source: "hostConfigs/puppet/init_jenkins.pp", destination: "/tmp/init_jenkins.pp"
+  config.vm.provision "file", source: "hostConfigs/puppet/puppet-master.conf", destination: "/tmp/puppet-master.conf"
+  config.vm.provision "file", source: "hostConfigs/puppet/.env", destination: "/tmp/env"
+
+  config.vm.provision :shell, path: "Vagrant.bootstrap.sh"
 
 
 end
